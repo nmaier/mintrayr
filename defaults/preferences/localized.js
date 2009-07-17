a6fad472c4671609060d1434f9f3f358d5eb746d
maierman <maierman@516452eb-20d2-40f5-96b4-ea0a29047bb5>
2009-07-17 21:18:28 +0000

    l10n: description and translator(s)

---
+++ defaults/preferences/localized.js
@@ -0,0 +1,3 @@
+pref('extensions.mintrayr@tn123.ath.cx.description', 'chrome://mintrayr/locale/localized.properties');
+pref('extensions.mintrayr@tn123.ath.cx.translator.1', 'chrome://mintrayr/locale/localized.properties');
+pref('extensions.mintrayr@tn123.ath.cx.translator.2', 'chrome://mintrayr/locale/localized.properties');
