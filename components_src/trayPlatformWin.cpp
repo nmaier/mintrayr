3f0351164e841b9b4d895fd287683a356c7a4fa6
maierman <maierman@516452eb-20d2-40f5-96b4-ea0a29047bb5>
2010-09-01 12:00:04 +0000

    Fix crasher

--- components_src/trayPlatformWin.cpp
+++ components_src/trayPlatformWin.cpp
@@ -101,11 +101,11 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
         }
 		case WM_WINDOWPOSCHANGED: {
 			/* XXX Fix this bit to something more reasonable
-				The following code kinda replicates the way mozilla gets the window state.
-				We intensionally "hide" the SW_SHOWMINIMIZED here.
-				This indeed might cause some side effects, but if it didn't we couldn't open
-				menus due to bugzilla #435848,.
-				This might defeat said bugfix completely reverting to old behavior, but only when we're active, of course.
+			  The following code kinda replicates the way mozilla gets the window state.
+			  We intensionally "hide" the SW_SHOWMINIMIZED here.
+			  This indeed might cause some side effects, but if it didn't we couldn't open
+			  menus due to bugzilla #435848,.
+			  This might defeat said bugfix completely reverting to old behavior, but only when we're active, of course.
 			*/
 			WINDOWPOS *wp = reinterpret_cast<WINDOWPOS*>(lParam);
 			if (wp->flags & SWP_FRAMECHANGED && ::IsWindowVisible(hwnd)) {
