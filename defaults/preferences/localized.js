43746fc6b7b938c7a882de130b92512017d75340
Nils Maier <maierman@web.de>
2010-09-01 17:35:45 +0200

    Localize addon descriptions via install.rdf
    Instead of preferences and properties file, that is.

--- defaults/preferences/localized.js
+++
@@ -1,3 +0,0 @@
-pref('extensions.mintrayr@tn123.ath.cx.description', 'chrome://mintrayr/locale/localized.properties');
-pref('extensions.mintrayr@tn123.ath.cx.translator.1', 'chrome://mintrayr/locale/localized.properties');
-pref('extensions.mintrayr@tn123.ath.cx.translator.2', 'chrome://mintrayr/locale/localized.properties');
