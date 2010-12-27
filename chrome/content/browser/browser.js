e7fc1079d505726e8058b2c7be9761939a2a76a1
Nils Maier <maierman@web.de>
2010-12-27 04:26:31 +0100

    Only suppress when watching browser windows
    (left unstaged, damnit)

--- chrome/content/browser/browser.js
+++ chrome/content/browser/browser.js
@@ -49,7 +49,8 @@ function MinTrayRBrowser() {
 		}
 		let _old = WindowIsClosing;
 		WindowIsClosing = function() {
-			if (self.prefs.getExt('minimizeon', 1) & (1<<1)) {
+			if (self.prefs.getExt('browser.watchbrowser', true)
+        && (self.prefs.getExt('minimizeon', 1) & (1<<1))) {
 				return false;
 			}
 			return _old.apply(window, arguments);
