e23b28705ef92c75aaf763ad9cf57a7ec10e7771
Nils Maier <maierman@web.de>
2012-11-21 06:16:17 +0100

    Fix Thunderbird soft-buttons integration

--- chrome/content/messenger/messenger.js
+++ chrome/content/messenger/messenger.js
@@ -6,6 +6,7 @@ var gMinTrayR = {};
 addEventListener(
   "load",
   function() {
+    function $(id) document.getElementById(id);
     removeEventListener("load", arguments.callee, true);
 
     Components.utils.import("resource://mintrayr/mintrayr.jsm", gMinTrayR);
@@ -23,6 +24,52 @@ addEventListener(
         catch (ex) {
           // no-op
         }
+
+        // Correctly tray-on-close, as Thunderbird 17 owner-draws the window controls in non-aero windows.
+        // This will remove the commands from the close button widgets and add an own window handler.
+        // Sucks, but there is not really another way.
+        (function(self) {
+          function MinTrayRTryCloseWindow(event){
+            if (self.prefs.getExt("messenger.watchmessenger", true)
+              && (self.prefs.getExt('minimizeon', 1) & (1<<1))) {
+              self.minimize();
+              event.preventDefault();
+              event.stopPropagation();
+              return false;
+            }
+            // must be in sync with the original command
+            return goQuitApplication();
+          }
+          function MinTrayRTryMinimizeWindow(event) {
+            if (self.prefs.getExt("messenger.watchmessenger", true)
+                && (self.prefs.getExt('minimizeon', 1) & (1<<0))) {
+                self.minimize();
+                event.preventDefault();
+                event.stopPropagation();
+                return false;
+              }
+              // must be in sync with the original command
+              return window.minimize();
+          }
+
+          function hijackButton(newCommand, id) {
+            let button = $(id);
+            if (!button) {
+              // Only available in Thunderbird 17
+              return;
+            }
+
+            // Remove old command(s)
+            button.removeAttribute('command');
+            button.removeAttribute('oncommand');
+
+            // Add ourselves
+            button.addEventListener('command', newCommand, false);
+          }
+          ['titlebar-close'].forEach(hijackButton.bind(null, MinTrayRTryCloseWindow));
+          ['titlebar-min'].forEach(hijackButton.bind(null, MinTrayRTryMinimizeWindow));
+        })(this);
+
         this.cloneToMenu('MinTrayR_sep-top', ["newNewMsgCmd", "button-getAllNewMsg", "addressBook"], false);
         this.cloneToMenu('MinTrayR_sep-bottom', ['menu_FileQuitItem'], true);
         document
